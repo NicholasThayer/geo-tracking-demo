99b34e50d4008006f02a801e0df0cac0445c04b7
NicholasThayer <thayer.nicholas@gmail.com>
2020-06-14 03:06:12 -0400

    fixed export

--- src/conf.js
+++ src/conf.js
@@ -1,4 +1,6 @@
+export const mapboxToken = 'pk.eyJ1IjoibmljaG9sYXN0aGF5ZXIiLCJhIjoiY2tiYWN3Zzl0MDRrYzJua29meWJqdHk5ZiJ9.oJ7k0uTbi3TXVdFpy3dpNw';
+
 export const conf = {
-  mapboxToken: 'pk.eyJ1IjoibmljaG9sYXN0aGF5ZXIiLCJhIjoiY2tiYWN3Zzl0MDRrYzJua29meWJqdHk5ZiJ9.oJ7k0uTbi3TXVdFpy3dpNw',
-  
+  mapboxToken
+
 }
